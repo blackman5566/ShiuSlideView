4e06eff77b854a353f6bb573dea0a869832dd129
Allen_shiu <pt7922310@gmail.com>
2016-02-02 15:42:35 +0800

    init。

---
+++ ShiuSlideView/MainViewController/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  ShiuSlideView
+//
+//  Created by 許佳豪 on 2016/2/2.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
