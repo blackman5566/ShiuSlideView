646aa4615d2860681cb069f82f4ae33359a9e345
Allen_shiu <pt7922310@gmail.com>
2016-02-02 15:25:21 +0800

    Initial Commit

---
+++ ShiuSlideView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShiuSlideView
+//
+//  Created by 許佳豪 on 2016/2/2.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShiuSlideView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShiuSlideView
+//
+//  Created by 許佳豪 on 2016/2/2.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
